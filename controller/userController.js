279864cef47ce9a6dfc8a31ae277936b664ec12c
Ch.Jashwanth <168354434+Jashu028@users.noreply.github.com>
2025-04-05 21:37:27 +0530

    Update userController.js

--- controller/userController.js
+++ controller/userController.js
@@ -177,14 +177,14 @@ const login = async (req, res) => {
     res.cookie("accessToken", accessToken, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "Strict",
+    sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
     maxAge: 15 * 60 * 1000,
   });
   
   res.cookie("refreshToken", refreshToken, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "Strict",
+    sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 
